713c604a1304d253af13cb4f4953e9e1934da4b5
Zachary Sunberg <sunbergzach@gmail.com>
2018-09-19 13:13:07 -0700

    compose glue better

--- src/compose_glue.jl
+++ src/compose_glue.jl
@@ -1,9 +1,17 @@
 using Compose
 
-function blink!(win::W, rendering::Compose.Context) where W <: WindowLike
+# this is just a workaround for https://github.com/GiovineItalia/Compose.jl/issues/304
+# it should be removed when that is fixed
+function blink!(win::Window, rendering::C) where C <: Compose.Container
     x, y = size(win)
     sz = min(x,y)
     s = SVG(0.98*sz*px, 0.98*sz*px)
+    draw(s, rendering)
+    blink!(win, s)
+end
+
+function blink!(win::TestWindow, rendering::C) where C <: Compose.Container
+    s = SVG(400px, 400px)
     draw(s, rendering)
     blink!(win, s)
 end
