f033cbe0edf90bed2a7bf42e25c1414bb35955b7
Zachary Sunberg <sunbergzach@gmail.com>
2019-07-03 13:00:28 -0700

    removed Compose integration hack

--- test/runtests.jl
+++ test/runtests.jl
@@ -21,3 +21,12 @@ bs = BlinkSimulator(window=TestWindow(),
                     max_fps=100
                    )
 simulate(bs, m)
+
+m = SimpleGridWorld()
+bs = BlinkSimulator(window=TestWindow(),
+                    extra_initial=true,
+                    extra_final=true,
+                    max_steps=10,
+                    max_fps=100
+                   )
+simulate(bs, m)
